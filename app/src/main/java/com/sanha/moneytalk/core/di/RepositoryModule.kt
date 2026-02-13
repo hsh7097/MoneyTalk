3a3b4dbbbbf9b426a2eb57229ddd47c517da9676
hasanghyun <hsh7097@naver.com>
2026-02-14 01:49:12 +0900

    Fix Hilt binding for GeminiRepository interface

--- app/src/main/java/com/sanha/moneytalk/core/di/RepositoryModule.kt
+++ app/src/main/java/com/sanha/moneytalk/core/di/RepositoryModule.kt
@@ -2,6 +2,8 @@ package com.sanha.moneytalk.core.di
 
 import com.sanha.moneytalk.feature.chat.data.ChatRepository
 import com.sanha.moneytalk.feature.chat.data.ChatRepositoryImpl
+import com.sanha.moneytalk.feature.chat.data.GeminiRepository
+import com.sanha.moneytalk.feature.chat.data.GeminiRepositoryImpl
 import com.sanha.moneytalk.feature.home.data.CategoryClassifierService
 import com.sanha.moneytalk.feature.home.data.CategoryClassifierServiceImpl
 import com.sanha.moneytalk.feature.home.data.GeminiCategoryRepository
@@ -21,6 +23,10 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindChatRepository(impl: ChatRepositoryImpl): ChatRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindGeminiRepository(impl: GeminiRepositoryImpl): GeminiRepository
 
     @Binds
     @Singleton
