014701d5e77055ae2eb6dd650505483b6fb83e37
hasanghyun <hsh70971@gmail.com>
2026-04-30 21:38:48 +0900

    versioncode up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         applicationId = "com.sanha.moneytalk"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
+        versionCode = 12
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
